6250db4397e838407820fc12d298b9e5afa75a7f
Lucas Oliveira <oliveiraborgeslucas@gmail.com>
2020-04-28 22:24:13 +0100

    refectoring

--- src/main/java/com/oliveiralucaspro/domain/Product.java
+++ src/main/java/com/oliveiralucaspro/domain/Product.java
@@ -6,9 +6,6 @@ import org.bson.types.ObjectId;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-/**
- * Created by jt on 1/10/17.
- */
 @Document
 public class Product {
     @Id
